92d252e25c8729ac8c91905a41baf38b13d24d5d
Romans Kuznecovs <kuznecovs.romans@gmail.com>
2024-02-06 23:45:16 +0000

    Refactor quiz implementation to remove QuizSection and have Quiz have a continent

--- shared/src/commonMain/kotlin/com/geotrainer/shared/feature/allquizzes/AllQuizzesUseCase.kt
+++ shared/src/commonMain/kotlin/com/geotrainer/shared/feature/allquizzes/AllQuizzesUseCase.kt
@@ -1,14 +1,17 @@
 package com.geotrainer.shared.feature.allquizzes
 
-import com.geotrainer.shared.model.quiz.QuizSection
+import com.geotrainer.shared.model.quiz.Quiz
 import com.geotrainer.shared.type.ApiResult
+import com.geotrainer.shared.type.mapSuccess
 
 internal interface AllQuizzesUseCase {
-    suspend fun getAllQuizSections(): ApiResult<List<QuizSection>>
+    suspend fun getAllQuizzes(): ApiResult<List<Quiz>>
 }
 
 internal class AllQuizzesUseCaseImpl(
     private val allQuizzesRepository: AllQuizzesRepository
 ) : AllQuizzesUseCase {
-    override suspend fun getAllQuizSections() = allQuizzesRepository.getAllQuizSections()
+    override suspend fun getAllQuizzes() = allQuizzesRepository.getAllQuizzes().mapSuccess { quizzes ->
+        quizzes.sortedBy { quiz -> quiz.title }
+    }
 }
--- shared/src/commonMain/kotlin/com/geotrainer/shared/model/quiz/Quiz.kt
+++ shared/src/commonMain/kotlin/com/geotrainer/shared/model/quiz/Quiz.kt
@@ -1,10 +1,12 @@
 package com.geotrainer.shared.model.quiz
 
+import com.geotrainer.shared.model.Continent
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Quiz(
     val quizId: String,
     val title: String,
-    val description: String
+    val description: String,
+    val continent: Continent?
 )
