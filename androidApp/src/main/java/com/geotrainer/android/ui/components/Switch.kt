2a0c840ad336960788a661457e66b87d0d345b90
James Bennett <2009bennettj@gmail.com>
2024-02-24 15:31:28 +0100

    Settings screen skeleton with Material3 switch

---
+++ androidApp/src/main/java/com/geotrainer/android/ui/components/Switch.kt
@@ -0,0 +1,72 @@
+package com.geotrainer.android.ui.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.geotrainer.android.ui.components.preview.PreviewSurface
+import com.geotrainer.android.ui.theme.GeoTrainerTheme
+
+private const val ENTER_ANIM_DURATION_MILLIS = 500
+private const val EXIT_ANIM_DURATION_MILLIS = 100
+
+@Composable
+fun PrimarySwitch(checked: Boolean, onCheckedChange: (Boolean) -> Unit) = Switch(
+    checked = checked,
+    onCheckedChange = onCheckedChange,
+    colors = SwitchDefaults.colors(
+        checkedThumbColor = GeoTrainerTheme.colors.White,
+        checkedTrackColor = GeoTrainerTheme.colors.DarkBlue,
+        uncheckedThumbColor = GeoTrainerTheme.colors.LightBlue,
+        uncheckedTrackColor = GeoTrainerTheme.colors.White,
+        uncheckedBorderColor = MaterialTheme.colorScheme.onSurface,
+        checkedBorderColor = GeoTrainerTheme.colors.DarkBlue,
+    ),
+    thumbContent = {
+        AnimatedVisibility(
+            visible = checked,
+            enter = fadeIn(animationSpec = tween(ENTER_ANIM_DURATION_MILLIS)),
+            exit = fadeOut(animationSpec = tween(EXIT_ANIM_DURATION_MILLIS))
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Check,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(24.dp)
+                    .padding(4.dp),
+                tint = GeoTrainerTheme.colors.DarkBlue,
+            )
+        }
+    }
+)
+
+@Preview
+@Composable
+fun PrimarySwitchPreview() = PreviewSurface(color = Color.White) {
+    var isChecked by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .padding(32.dp)
+    ) {
+        PrimarySwitch(checked = isChecked, onCheckedChange = { isChecked = it })
+        PrimarySwitch(checked = !isChecked, onCheckedChange = { isChecked = it })
+    }
+}
--- androidApp/src/main/java/com/geotrainer/android/ui/screens/settings/SettingsScreen.kt
+++ androidApp/src/main/java/com/geotrainer/android/ui/screens/settings/SettingsScreen.kt
@@ -1,15 +1,49 @@
 package com.geotrainer.android.ui.screens.settings
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.geotrainer.android.R
+import com.geotrainer.android.ui.components.NavigationBarIconsColor
+import com.geotrainer.android.ui.components.PrimarySwitch
+import com.geotrainer.android.ui.components.Screen
+import com.geotrainer.android.ui.components.ScreenSlotBasicScrollable
+import com.geotrainer.android.ui.components.StatusBarIconsColor
+import com.geotrainer.android.ui.components.SystemBarIconsColor
 import com.geotrainer.android.ui.components.navigation.FadeTransitions
 import com.geotrainer.android.ui.components.navigation.navgraphs.SettingsNavGraph
+import com.geotrainer.android.ui.components.preview.PreviewSurface
+import com.geotrainer.android.ui.theme.GeoTrainerTheme
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+private const val previewGroup = "Settings"
 
 @SettingsNavGraph(start = true)
 @Destination(style = FadeTransitions::class)
@@ -17,7 +51,176 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 fun SettingsScreen(
     navigator: DestinationsNavigator,
 ) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Settings")
+    Screen(
+        systemBarIconsColor = SystemBarIconsColor(
+            statusBar = StatusBarIconsColor.Dark,
+            navigationBar = NavigationBarIconsColor.Light
+        )
+    ) {
+        SettingsContent(
+            onQuizSettings = { /* TODO */ },
+            onQuizStatistics = { /* TODO */ },
+            onContact = { /* TODO */ },
+            onReportIssue = { /* TODO */ },
+            onPrivacyPolicy = { /* TODO */ },
+            onLicensingInformation = { /* TODO */ },
+            onSources = { /* TODO */ },
+            onAbout = { /* TODO */ },
+        )
     }
+}
+
+@Composable
+fun SettingsContent(
+    onQuizSettings: () -> Unit,
+    onQuizStatistics: () -> Unit,
+    onContact: () -> Unit,
+    onReportIssue: () -> Unit,
+    onPrivacyPolicy: () -> Unit,
+    onLicensingInformation: () -> Unit,
+    onSources: () -> Unit,
+    onAbout: () -> Unit,
+) = ScreenSlotBasicScrollable(contentModifier = Modifier.padding(16.dp)) {
+    Text(
+        text = "Settings",
+        style = MaterialTheme.typography.headlineLarge,
+        fontWeight = FontWeight.Bold
+    )
+    Spacer(modifier = Modifier.height(32.dp))
+
+    QuizSection(onQuizSettings, onQuizStatistics)
+    CommunicationSection(onContact, onReportIssue)
+    LegalSection(onPrivacyPolicy, onLicensingInformation, onSources)
+    AboutSection(onAbout)
+}
+
+@Composable
+fun SettingsSectionContainer(content: @Composable ColumnScope.() -> Unit) = Column(
+    modifier = Modifier
+        .padding(top = 8.dp, bottom = 16.dp)
+        .clip(RoundedCornerShape(8.dp))
+        .background(GeoTrainerTheme.colors.White, shape = RoundedCornerShape(8.dp)),
+    content = content
+)
+
+@Composable
+fun SettingsItem(
+    text: String,
+    onClick: () -> Unit,
+    trailing: @Composable (() -> Unit)? = null,
+) = Row(
+    modifier = Modifier
+        .fillMaxWidth()
+        .clickable(onClick = onClick)
+        .sizeIn(minHeight = 48.dp)
+        .padding(horizontal = 8.dp),
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.SpaceBetween
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyMedium,
+    )
+    when {
+        trailing != null -> trailing()
+        else -> Icon(
+            painter = painterResource(id = R.drawable.ic_chevron_right),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .size(24.dp),
+            tint = GeoTrainerTheme.colors.DarkBlue,
+        )
+    }
+}
+
+@Composable
+fun QuizSection(
+    onQuizSettings: () -> Unit,
+    onQuizStatistics: () -> Unit,
+) = Column {
+    Text(
+        text = "Quiz",
+        style = MaterialTheme.typography.headlineSmall,
+    )
+    SettingsSectionContainer {
+        SettingsItem(text = "Quiz settings", onQuizSettings)
+        HorizontalDivider()
+        SettingsItem(text = "Statistics", onQuizStatistics)
+    }
+}
+
+@Composable
+fun CommunicationSection(
+    onContact: () -> Unit,
+    onReportIssue: () -> Unit,
+) = Column {
+    var isPushNotificationToggled by remember { mutableStateOf(false) }
+    Text(
+        text = "Communication",
+        style = MaterialTheme.typography.headlineSmall,
+    )
+    SettingsSectionContainer {
+        SettingsItem(
+            text = "Push notifications",
+            onClick = {
+                isPushNotificationToggled = !isPushNotificationToggled
+            },
+            trailing = {
+                PrimarySwitch(
+                    checked = isPushNotificationToggled,
+                    onCheckedChange = { isPushNotificationToggled = it }
+                )
+            }
+        )
+        HorizontalDivider()
+        SettingsItem(text = "Contact", onContact)
+        HorizontalDivider()
+        SettingsItem(text = "Report an issue", onReportIssue)
+    }
+}
+
+@Composable
+fun LegalSection(
+    onPrivacyPolicy: () -> Unit,
+    onLicensingInformation: () -> Unit,
+    onSources: () -> Unit,
+) = Column {
+    Text(
+        text = "Legal",
+        style = MaterialTheme.typography.headlineSmall,
+    )
+    SettingsSectionContainer {
+        SettingsItem(text = "Privacy policy", onPrivacyPolicy)
+        HorizontalDivider()
+        SettingsItem(text = "Licensing information", onLicensingInformation)
+        HorizontalDivider()
+        SettingsItem(text = "Sources", onSources)
+    }
+}
+
+@Composable
+fun AboutSection(onAbout: () -> Unit) = Column {
+    Text(
+        text = "About",
+        style = MaterialTheme.typography.headlineSmall,
+    )
+    SettingsSectionContainer {
+        SettingsItem(text = "About authors", onAbout)
+    }
+}
+
+@Preview(name = "Main Content", group = previewGroup)
+@Composable
+fun SettingsScreenContentPreview() = PreviewSurface {
+    SettingsContent(
+        onQuizSettings = {},
+        onQuizStatistics = {},
+        onContact = {},
+        onReportIssue = {},
+        onPrivacyPolicy = {},
+        onLicensingInformation = {},
+        onSources = {},
+        onAbout = {},
+    )
 }
